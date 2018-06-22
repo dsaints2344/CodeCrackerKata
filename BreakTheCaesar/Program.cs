3f0a4b1f97c53a2ba3755131d6bda5dc53c6dd11
David De Los Santos <ddelossantosmateo@gmail.com>
2018-06-22 02:19:28 -0400

    Uploading Version 1.0

---
+++ BreakTheCaesar/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BreakTheCaesar
+{
+
+    public class CipherSolver
+    {
+        public Dictionary<String, int> alphabetValues = new Dictionary<string, int>
+        {
+            {"A", 0 }, {"B", 1 }, {"C", 2 }, {"D", 3 }, {"E", 4 }, {"F", 5}, {"G", 6 },
+            {"H", 7 }, {"I", 8 }, {"J", 9}, {"K", 10}, {"L", 11 }, {"M", 12 }, {"N", 13 },
+            {"O", 14 }, {"P", 15 }, {"Q", 16 }, {"R", 17 }, {"S", 18 }, {"T", 19 }, {"U", 20 },
+            {"V", 21 }, {"W", 22 }, {"X", 23 }, {"Y", 24 }, {"Z", 25 }
+        };
+
+        public Dictionary<int, char> integersValues = new Dictionary<int, char>
+        {
+            {0, 'A' }, {1, 'B'}, {2, 'C'}, {3, 'D'}, {4, 'E'}, {5, 'F'}, {6, 'G'}, {7, 'H'},
+            {8, 'I' }, {9, 'J'}, {10, 'K'}, {11, 'L'}, {12, 'M'}, {13, 'N'}, {14, 'O'}, {15, 'P'},
+            {16, 'Q'}, {17, 'R'}, {18, 'S'}, {19, 'T'}, {20, 'U'}, {21, 'V'}, {22, 'W'}, {23, 'X'},
+            {24, 'Y' }, {25, 'Z'}
+        };
+
+        public int[] ConvertStringToNumbers(string input)
+        {
+
+            int[] in_numbers;
+            char[] in_letter;
+
+            string[] wordsNoSpecialChar = Regex.Split(input.ToUpper(), @"\W+");
+            in_letter = String.Join(String.Empty, wordsNoSpecialChar).ToCharArray();
+            in_numbers = new int[in_letter.Length];
+
+            for (int i = 0; i < in_letter.Length; i++)
+            {
+                string letter = Convert.ToString(in_letter[i]);
+                in_numbers[i] = alphabetValues[letter];
+            }
+
+            return in_numbers;
+
+
+
+        }
+
+        public int[] EncodeNumbers(int[] numbers, int shift)
+        {
+            int[] codedNumbers = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                codedNumbers[i] = (numbers[i] + shift) % 26;
+            }
+            return codedNumbers;
+        }
+
+        public String EncryptedWords(string input, int shift)
+        {
+            int[] numbersToEncrypt = ConvertStringToNumbers(input);
+            int[] encodedNumbers = EncodeNumbers(numbersToEncrypt, shift);
+
+            char[] word = new char[numbersToEncrypt.Length];
+
+            string temp = "";
+            string result = temp;
+
+            for (int i = 0; i < encodedNumbers.Length; i++)
+            {
+                word[i] = integersValues[encodedNumbers[i]];
+            }
+            temp = new string(word);
+
+            for (int j = 0; j < input.Length; j++)
+            {
+                if (char.IsWhiteSpace(input[j]))
+                {
+                    result = temp.Insert(j, " ");
+                }
+            }
+            String finalResult = CheckingPunctuations(result, input);
+            return finalResult;
+        }
+
+        public String CheckingPunctuations(string input, string original)
+        {
+            String result = "";
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (char.IsPunctuation(original[i]))
+                {
+                    result = input.Insert(i, original[i].ToString());
+                }
+            }
+
+            if (result.Length == 0)
+            {
+                result = input;
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CipherSolver c = new CipherSolver();
+        }
+    }
+}
---
+++ CodeCrackerTests/UnitTest1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BreakTheCaesar;
+
+namespace CodeCrackerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReturnNumberOfLetter()
+        {
+            string input = "A";
+
+            int[] expected = { 0 };
+
+            CipherSolver c = new CipherSolver();
+
+            var actualOutput = c.ConvertStringToNumbers(input);
+
+            CollectionAssert.AreEqual(expected, actualOutput);
+
+        }
+
+        [TestMethod]
+        public void ReturnNumberOfLowerLetter()
+        {
+            string input = "a";
+            int[] expected = { 0 };
+
+            CipherSolver c = new CipherSolver();
+
+            var actualOutput = c.ConvertStringToNumbers(input);
+            CollectionAssert.AreEqual(expected, actualOutput);
+
+        }
+
+        [TestMethod]
+        public void ReturnNumbersOfWord()
+        {
+            string input = "hola";
+            int[] expected = { 7, 14, 11, 0 };
+
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.ConvertStringToNumbers(input);
+
+            CollectionAssert.AreEqual(expected, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReturnNumberOfWordsWithSpace()
+        {
+            string input = "hola a";
+            int[] expected = { 7, 14, 11, 0, 0 };
+
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.ConvertStringToNumbers(input);
+
+            CollectionAssert.AreEqual(expected, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReturnNumbersOfWordsWithSpecialChar()
+        {
+            string input = "hola a.";
+            int[] expected = { 7, 14, 11, 0, 0 };
+
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.ConvertStringToNumbers(input);
+
+            CollectionAssert.AreEqual(expected, actualOutput);
+        }
+
+        [TestMethod]
+        public void EncodeNumbers()
+        {
+            //word: hello
+            int[] input = { 7, 4, 11, 11, 14 };
+
+            int[] expected = { 10, 7, 14, 14, 17 };
+            // Amount of letter's shift
+            int shiftKey = 3;
+            CipherSolver c = new CipherSolver();
+
+            var actualOutput = c.EncodeNumbers(input, shiftKey);
+
+            CollectionAssert.AreEqual(expected, actualOutput);
+        }
+
+        [TestMethod]
+        public void EncodeNumbersGreaterTwentyFive()
+        {
+            //letters: XYZ
+            int[] input = { 23, 24, 25 };
+
+            //letter's shift
+            int shift = 6;
+
+            int[] expected = { 3, 4, 5 };
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.EncodeNumbers(input, shift);
+
+            CollectionAssert.AreEqual(expected, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReturnEncryptedWord()
+        {
+            string input = "hello";
+            String expected = "IFMMP";
+
+            int shift = 1;
+
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.EncryptedWords(input, shift);
+
+            Assert.AreEqual(expected, actualOutput);
+
+
+        }
+
+        [TestMethod]
+        public void ReturnEncryptedWords()
+        {
+            string input = "hello Sir";
+            string expected = "IFMMP TJS";
+
+            int shift = 1;
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.EncryptedWords(input, shift);
+
+            Assert.AreEqual(expected, actualOutput);
+        }
+
+        [TestMethod]
+        public void ReturnEncryptedWordsWithPunctuation()
+        {
+            string input = "Hello There.";
+            string resultWithNoPunctuation = "IFMMP UIFSF";
+
+            string expected = "IFMMP UIFSF.";
+
+            CipherSolver c = new CipherSolver();
+            var actualOutput = c.CheckingPunctuations(resultWithNoPunctuation, input);
+
+            Assert.AreEqual(expected, actualOutput);
+        }
+
+    }
+}
